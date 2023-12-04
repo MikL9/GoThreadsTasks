779700d33c9cff2b01f8410369a92867e5d27cf5
MikL9 <mikl9@vk.com>
2023-12-04 09:52:27 +0300

    lab2

--- lab2/main.go
+++ lab2/main.go
@@ -1,81 +1,114 @@
 package main
 
-//
-//import (
-//	"fmt"
-//	"math/rand"
-//	"sync"
-//	"time"
-//)
-//
-//func countUniqueElements(arr []int, resultChan chan<- []int, wg *sync.WaitGroup) {
-//	defer wg.Done()
-//
-//	// Создаем карту для отслеживания уникальных элементов
-//	uniqueElements := make(map[int]struct{})
-//
-//	// Подсчитываем уникальные элементы в массиве
-//	for _, val := range arr {
-//		uniqueElements[val] = struct{}{}
-//	}
-//
-//	// Преобразуем уникальные элементы из карты в срез
-//	result := make([]int, 0, len(uniqueElements))
-//	for key := range uniqueElements {
-//		result = append(result, key)
-//	}
-//
-//	// Отправляем уникальные элементы в канал
-//	resultChan <- result
-//}
-//
-//func main() {
-//	// Ваш массив из 12 значений (пример случайных данных)
-//	arr := generateRandomArray(12, 1)
-//	arr = gaussianFilter(arr)
-//
-//	// Количество потоков
-//	numThreads := 4
-//
-//	// Канал для передачи результатов от горутин
-//	resultChan := make(chan []int, numThreads)
-//
-//	// Группа ожидания для дожидания завершения всех горутин
-//	var wg sync.WaitGroup
-//
-//	// Разбиваем массив на части и обрабатываем каждую часть в отдельной горутине
-//	for i := 0; i < numThreads; i++ {
-//		wg.Add(1)
-//		startIndex := i * (len(arr) / numThreads)
-//		endIndex := (i + 1) * (len(arr) / numThreads)
-//		go countUniqueElements(arr[startIndex:endIndex], resultChan, &wg)
-//	}
-//
-//	// Горутина для закрытия канала после завершения всех горутин
-//	go func() {
-//		wg.Wait()
-//		close(resultChan)
-//	}()
-//
-//	// Карта для подсчета общего количества уникальных элементов
-//	totalUniqueElements := make(map[int]struct{})
-//
-//	// Обработка результатов
-//	for result := range resultChan {
-//		for _, val := range result {
-//			totalUniqueElements[val] = struct{}{}
-//		}
-//	}
-//
-//	// Выводим общее количество уникальных элементов
-//	fmt.Println("Total unique elements:", len(totalUniqueElements))
-//}
-//
-//func generateRandomArray(size, M int) []int {
-//	array := make([]int, size)
-//	rand.Seed(time.Now().UnixNano())
-//	for i := 0; i < size; i++ {
-//		array[i] = rand.Intn(2*M+1) - M
-//	}
-//	return array
-//}
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+	"unsafe"
+)
+
+func main() {
+	arraySize := 100000000
+	data := generateRandomArray(arraySize)
+
+	randomMultiplier := rand.Intn(5) + 1
+
+	// Используем общую переменную для агрегации результатов
+	var resultCommon int
+	startTimeCommon := time.Now()
+	processArrayCommon(data, &resultCommon, randomMultiplier)
+	commonTime := time.Since(startTimeCommon)
+	fmt.Println("Метод 1 (с использованием общей переменной):")
+	fmt.Println("Результат:", resultCommon)
+	fmt.Println("Время выполнения:", commonTime)
+
+	// Используем атомарную переменную для агрегации результатов
+	var resultAtomic int64
+	startTimeAtomic := time.Now()
+	processArrayAtomic(data, &resultAtomic, randomMultiplier)
+	atomicTime := time.Since(startTimeAtomic)
+	fmt.Println("\nМетод 2 (с использованием атомарных операций):")
+	fmt.Println("Результат:", atomic.LoadInt64(&resultAtomic))
+	fmt.Println("Время выполнения:", atomicTime)
+
+	// Используем индивидуальные переменные для каждой горутины
+	var resultIndividuals []int
+	startTimeIndividual := time.Now()
+	processArrayIndividual(data, &resultIndividuals, randomMultiplier)
+	individualTime := time.Since(startTimeIndividual)
+	fmt.Println("\nМетод 4 (с использованием индивидуальных переменных для каждого потока):")
+	fmt.Println("Результат:", resultIndividuals[:10])
+	fmt.Println("Время выполнения:", individualTime)
+
+	// Используем мьютекс для многопоточного метода
+	var resultMutexConcurrent int
+	var mutexConcurrent sync.Mutex
+	startTimeMutexConcurrent := time.Now()
+	numThreads := 4
+	processArrayMutexConcurrent(data, &resultMutexConcurrent, &mutexConcurrent, randomMultiplier, numThreads)
+	mutexConcurrentTime := time.Since(startTimeMutexConcurrent)
+	fmt.Println("\nМетод 5 (многопоточный метод с использованием мьютекса):")
+	fmt.Println("Результат:", resultMutexConcurrent)
+	fmt.Println("Время выполнения:", mutexConcurrentTime)
+}
+
+// Генерация случайного массива
+func generateRandomArray(size int) []int {
+	rand.Seed(time.Now().UnixNano())
+	array := make([]int, size)
+	for i := range array {
+		array[i] = rand.Intn(100)
+	}
+	return array
+}
+
+// Умножение каждого элемента массива на случайное число с использованием общей переменной
+func processArrayCommon(data []int, result *int, randomMultiplier int) {
+	for _, value := range data {
+		*result += value * randomMultiplier
+	}
+}
+
+// Умножение каждого элемента массива на случайное число с использованием атомарных операций
+func processArrayAtomic(data []int, result *int64, randomMultiplier int) {
+	for _, value := range data {
+		atomic.AddInt64((*int64)(unsafe.Pointer(result)), int64(value*randomMultiplier))
+	}
+}
+
+// Умножение каждого элемента массива на случайное число с использованием индивидуальных переменных для каждого потока
+func processArrayIndividual(data []int, result *[]int, randomMultiplier int) {
+	localResults := make([]int, len(data))
+	for i, value := range data {
+		localResults[i] = value * randomMultiplier
+	}
+	*result = localResults
+}
+
+func processArrayMutexConcurrent(data []int, result *int, mutex *sync.Mutex, randomMultiplier int, numThreads int) {
+	var wg sync.WaitGroup
+	wg.Add(numThreads)
+
+	chunkSize := len(data) / numThreads
+
+	for i := 0; i < numThreads; i++ {
+		startIndex := i * chunkSize
+		endIndex := (i + 1) * chunkSize
+
+		go func(start, end int) {
+			defer wg.Done()
+			localResult := 0
+			for j := start; j < end; j++ {
+				localResult += data[j] * randomMultiplier
+			}
+
+			mutex.Lock()
+			*result += localResult
+			mutex.Unlock()
+		}(startIndex, endIndex)
+	}
+
+	wg.Wait()
+}
